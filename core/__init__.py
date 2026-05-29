9598bf0c77806580d9bbb6f0fbb4627bbceb2d4c
Hardy-wenshy <3352990378@qq.com>
2026-05-29 23:30:07 +0800

    merge remote repository

--- core/__init__.py
+++ core/__init__.py
@@ -3,7 +3,7 @@ Core computational modules for liver spatial axis analysis
 """
 
 from .marker_score import compute_marker_scores,compute_net_score
-from .transitiaon_matrix import bulid_knn,bulid_dual_transtion_metrices
+from .transition_matrix import bulid_knn,build_dual_transition_matrices
 from .seed_detection import detect_seeds_dynamic
 from .bidirectional_axis import compute_bidirectional_axis
 from .folw_analysis import compute_local_flow,local_gradient_strength
